9867eb693a9c28f21289277e0d7ebbfd123ff0e2
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2024-11-07 10:19:19 -0300

    07-11-2024

--- src/components/launch.js
+++ src/components/launch.js
@@ -66,7 +66,7 @@ import backgroundLive from '../assets/images/backgrond-launch-live.gif'
                 </p>
                     <div className='videoLiveInicio'>
                       <ReactPlayer className="watchVideo" scrolling="no" frameborder="0" onload="iFrameResize()" 
-                      url={`https://fb.watch/vFVeJrnZ1J/`} controls='true'/>
+                      url={`https://fb.watch/vItSPtuPpk/`} controls='true'/>
                     </div>
             </div>
           </div>
